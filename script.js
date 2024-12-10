233edb8efe383aa76a340b29f1fd76e02f06e4d4
Null <batubutu2077@gmail.com>
2024-12-10 19:57:17 +0400

    fix issues

---
+++ script.js
@@ -0,0 +1,50 @@
+var swiper = new Swiper(".heroSwiper", {
+    loop: true,
+    navigation: {
+        nextEl: ".hero-swiper__button-next",
+        prevEl: ".hero-swiper__button-prev",
+    },
+
+});
+//End hero swiper
+
+var swiper = new Swiper(".brandSwiper", {
+    spaceBetween: 30,
+    slidesPerView: 3,
+    loop: true,
+    pagination: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+        clickable: true,
+    },
+    breakpoints: {
+        990: {
+            slidesPerView: 5,
+        },
+
+        575: {
+            slidesPerView: 4,
+        },
+
+    },
+});
+//End brand swiper
+
+function openVideo() {
+    const videoModal = document.getElementById('video-box__modal');
+    const youtubeVideo = document.getElementById('youtube-video');
+
+    const youtubeLink = 'https://www.youtube.com/watch?v=fkoEj95puX0';
+
+    youtubeVideo.src = youtubeLink + "?autoplay=1";
+    videoModal.style.display = 'flex';
+}
+
+function closeVideo() {
+    const videoModal = document.getElementById('video-modal');
+    const youtubeVideo = document.getElementById('youtube-video');
+
+    youtubeVideo.src = "";
+    videoModal.style.display = 'none';
+}
+//End youtube video modal
